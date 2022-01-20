49f5fb490ce11e0bcd649e7e4fa008063088e8c9
glitjch <t.jamesphan@gmail.com>
2022-01-20 01:35:03 +0000

    adding string messages to send to others

--- input.js
+++ input.js
@@ -1,9 +1,7 @@
-const { connect } = require("./client");
-
 let connection;
 
 
-const handleUserInput = function (key) {
+const handleUserInput = function(key) {
   // Exit the server (not just the terminal)
   if (key === '\u0003') {
     console.log("I'm outta here.");
@@ -13,32 +11,53 @@ const handleUserInput = function (key) {
   // Directional commands
   // Up W key
   if (key === `\u0077`) {
-    connection.write("Move: up")
+    connection.write("Move: up");
     // console.log("up");
   }
   // Left A key
   if (key === '\u0061') {
-    connection.write("Move: left")
+    connection.write("Move: left");
     // console.log("left");
   }
   // Down S key
   if (key === '\u0073') {
-    connection.write("Move: down")
+    connection.write("Move: down");
     // console.log("down");
   }
   // Right D key
   if (key === '\u0064') {
-    connection.write("Move: right")
+    connection.write("Move: right");
     // console.log("right");
   }
+
+  // Press Q to send message
+  if (key === "\u0071") {
+    connection.write(`Say: Hello mudda.`);
+  }
+
+  // Press E to send message
+  if (key === "\u0065") {
+    connection.write(`Say: Hello fadda.`);
+  }
+
+  // Press Z to send message
+  if (key === "\u007A") {
+    connection.write(`Say: Here I am at.`);
+  }
+
+  // Press C to send message
+  if (key === "\u0063") {
+    connection.write(`Say: Camp Grenada.`);
+  }
+  // create a special key to send messages using Say:
 };
 
 
 
 
-const setupInput = function (conn) {
+const setupInput = function(conn) {
   connection = conn;
-  
+
   const stdin = process.stdin;
   stdin.setRawMode(true);
   stdin.setEncoding("utf8");
